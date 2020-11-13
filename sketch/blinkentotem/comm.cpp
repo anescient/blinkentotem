61c352946791067d451e2f74f37f987d33a97cbd
anescient <anescient@gmail.com>
2020-11-13 07:57:14 -0600

    use long serial timeout where data is expected

--- sketch/blinkentotem/comm.cpp
+++ sketch/blinkentotem/comm.cpp
@@ -7,91 +7,94 @@ void Comm::begin() {
 }
 
 datatype_t Comm::receive(int timeoutms) {
+  Serial.setTimeout(timeoutms);
+  if(!Serial.find('$'))
+    return NONE;
+
+  Serial.setTimeout(500);
+  size_t headersize = Serial.readBytesUntil('\0', buffer.bytes, COMM_BUFFER_SIZE);
+  if(headersize == 0)
+    return NONE;
+
   datatype_t datatype = NONE;
   size_t datasize = 0;
-  Serial.setTimeout(timeoutms);
-  if(Serial.find('$')) {
-    size_t headersize = Serial.readBytesUntil('\0', buffer.bytes, COMM_BUFFER_SIZE);
-    if(headersize == 0)
-      return NONE;
-    switch(buffer.command) {
-      case ' ':
-        datatype = PING;
-        break;
+  switch(buffer.command) {
+    case ' ':
+      datatype = PING;
+      break;
 
-      case ';':
-        datatype = FLUSH;
-        break;
+    case ';':
+      datatype = FLUSH;
+      break;
 
-      case 'c':
-        datatype = CLEAR;
-        break;
+    case 'c':
+      datatype = CLEAR;
+      break;
 
-      case 'p':
-        datatype = CONFIG;
-        datasize = sizeof(config_t);
-        break;
+    case 'p':
+      datatype = CONFIG;
+      datasize = sizeof(config_t);
+      break;
 
-      case '1':
-        datatype = RGBFRAME;
-        datasize = RGB_COUNT * sizeof(rgb_t);
-        break;
+    case '1':
+      datatype = RGBFRAME;
+      datasize = RGB_COUNT * sizeof(rgb_t);
+      break;
 
-      case 'd':
-        datatype = DRUM;
-        datasize = DRUM_COUNT * sizeof(rgb_t);
-        break;
+    case 'd':
+      datatype = DRUM;
+      datasize = DRUM_COUNT * sizeof(rgb_t);
+      break;
 
-      case 'r':
-        datatype = RAID;
-        datasize = RAID_COUNT * sizeof(rgb_t);
-        break;
+    case 'r':
+      datatype = RAID;
+      datasize = RAID_COUNT * sizeof(rgb_t);
+      break;
 
-      case 'l':
-        datatype = LAMP;
-        datasize = LAMP_COUNT * sizeof(rgb_t);
-        break;
+    case 'l':
+      datatype = LAMP;
+      datasize = LAMP_COUNT * sizeof(rgb_t);
+      break;
 
-      case 'f':
-        datatype = RAIDFLASH;
-        datasize = RAID_COUNT * sizeof(iopulse_t);
-        break;
+    case 'f':
+      datatype = RAIDFLASH;
+      datasize = RAID_COUNT * sizeof(iopulse_t);
+      break;
 
-      case 'm':
-        datatype = DRUMFLASH;
-        datasize = sizeof(iopulse_t);
-        break;
+    case 'm':
+      datatype = DRUMFLASH;
+      datasize = sizeof(iopulse_t);
+      break;
 
-      case '2':
-        datatype = RGBWFRAME;
-        datasize = RGBW_COUNT * sizeof(rgbw_t);
-        break;
+    case '2':
+      datatype = RGBWFRAME;
+      datasize = RGBW_COUNT * sizeof(rgbw_t);
+      break;
 
-      case 'g':
-        datatype = RGBW_FADE_R;
-        datasize = RGBW_COUNT * sizeof(fade_t);
-        break;
+    case 'g':
+      datatype = RGBW_FADE_R;
+      datasize = RGBW_COUNT * sizeof(fade_t);
+      break;
 
-      case 'i':
-        datatype = RGBW_FADE_G;
-        datasize = RGBW_COUNT * sizeof(fade_t);
-        break;
+    case 'i':
+      datatype = RGBW_FADE_G;
+      datasize = RGBW_COUNT * sizeof(fade_t);
+      break;
 
-      case 's':
-        datatype = RGBW_SPIN_B;
-        datasize = RGBW_COUNT * sizeof(spin_t);
-        break;
+    case 's':
+      datatype = RGBW_SPIN_B;
+      datasize = RGBW_COUNT * sizeof(spin_t);
+      break;
 
-      case 'z':
-        datatype = RGBW_FADE_W;
-        datasize = RGBW_COUNT * sizeof(fade_t);
-        break;
-    }
+    case 'z':
+      datatype = RGBW_FADE_W;
+      datasize = RGBW_COUNT * sizeof(fade_t);
+      break;
+  }
 
-    if(datasize > 0 && Serial.readBytes(buffer.bytes, datasize) != datasize) {
-      datatype = NONE;
-      digitalWrite(13, HIGH);
-    }
+  if(datasize > 0 && Serial.readBytes(buffer.bytes, datasize) != datasize) {
+    datatype = NONE;
+    digitalWrite(13, HIGH);
   }
   return datatype;
 }
